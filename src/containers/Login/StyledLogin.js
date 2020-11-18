925096a3f168d45cdad34bc64733d9f9c6cd66f6
The Nuraryhion <zadruzynskimateusz@gmail.com>
2020-11-18 19:12:04 +0100

    Login

---
+++ src/containers/Login/StyledLogin.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import backgroundImage from "../../assets/backgrounds/support-wave.svg";
+export const StyledSupport = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: #303030 url(${backgroundImage});
+  background-size: cover;
+  color: white;
+`;
+
+export const Container = styled.div`
+  width: 5rem;
+  font-size: 1rem;
+  margin-left: 45%;
+  height: 2.5rem; 
+  padding-top: 17 %;
+  border: none;
+`;
