d3f64539741a758644d2de50588bfdb8e3713ff6
Kacper <kacdab003@utp.edu.pl>
2020-12-03 02:26:20 +0100

    Welcome Screen - Add topbar with log in button

--- src/containers/WelcomeScreen/StyledWelcomeScreen.js
+++ src/containers/WelcomeScreen/StyledWelcomeScreen.js
@@ -4,6 +4,7 @@ export const WelcomeScreenContainer = styled.div`
   width: 100%;
   height: 100%;
   display: grid;
+  position: relative;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr;
   align-content: center;
