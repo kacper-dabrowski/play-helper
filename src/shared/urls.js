de25b1d057e845bd52ce0e5883e30cf173ae19da
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-02 10:10:17 +0100

    Urls - Add production url

--- src/shared/urls.js
+++ src/shared/urls.js
@@ -1,5 +1,8 @@
 const urls = {
-  api: process.env.NODE_ENV === "production" ? "" : "http://localhost:3001",
+  api:
+    process.env.NODE_ENV === "production"
+      ? "http://play-helper-api.herokuapp.com/"
+      : "http://localhost:3001",
   signup: "/signup",
   login: "/login",
 };
