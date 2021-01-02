8df6a2026198d958e8277b5e252dfc320b2020c3
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-02 08:16:42 +0100

    Error message - Add border

--- src/components/Modals/ErrorMessage/StyledErrorMessage.js
+++ src/components/Modals/ErrorMessage/StyledErrorMessage.js
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 export const ErrorMessageContainer = styled.div`
   display: flex;
-  padding: 0.5rem;
 `;
 
 export const StyledErrorMessage = styled.p`
+  padding: 0.5rem;
+  border: pink 1px solid;
   color: pink;
   text-align: center;
   margin: 0 auto;
