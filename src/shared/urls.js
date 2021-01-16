f7356be77118f63ad54d0ad7e9fa61afb5ccd989
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-16 14:09:43 +0100

    UseRequest - Add useRequest hook instead of useEffect

--- src/shared/urls.js
+++ src/shared/urls.js
@@ -1,11 +1,12 @@
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://play-helper-api.herokuapp.com"
+    : "http://localhost:3001";
+
 const urls = {
-  api:
-    process.env.NODE_ENV === "production"
-      ? "https://play-helper-api.herokuapp.com"
-      : "http://localhost:3001",
-  signup: "/signup",
-  login: "/login",
-  srq: "/srq",
+  signup: API_URL + "/signup",
+  login: API_URL + "/login",
+  srq: API_URL + "/srq",
 };
 
 export default urls;
