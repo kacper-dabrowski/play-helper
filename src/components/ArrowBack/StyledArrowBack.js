85ee54016ace4032660704d907aa8d32c0317745
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-18 18:33:53 +0100

    Change arrows

--- src/components/ArrowBack/StyledArrowBack.js
+++ src/components/ArrowBack/StyledArrowBack.js
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import nextArrow from "../../assets/icons/next-back-arrow.svg";
-import supportArrow from "../../assets/icons/support-back-arrow.svg";
+import nextArrow from "../../assets/icons/arrow-back-right.svg";
+import supportArrow from "../../assets/icons/arrow-back-left.svg";
 
 const chooseArrow = (props) => {
   return props.type === "NEXT" ? nextArrow : supportArrow;
 };
 export const StyledArrowBack = styled(Link)`
   margin: 1rem;
-  width: 3rem;
-  height: 3rem;
+  width: 2rem;
+  height: 2rem;
   background-image: url(${(props) => chooseArrow(props)});
   background-size: cover;
+  transition: ease-in-out 0.2s;
+  &:hover {
+    transform: scale(1.1);
+  }
 `;
