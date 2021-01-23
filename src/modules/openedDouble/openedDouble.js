1901125182fe790e2412c4fcacd4b9549a5f0f0b
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-23 17:10:00 +0100

    #2 - Change the order of doubles

--- src/modules/openedDouble/openedDouble.js
+++ src/modules/openedDouble/openedDouble.js
@@ -1,23 +1,23 @@
 import config from "../../shared/identifiers";
 
-export const generateOpenedDoubleTemplate = (
+export const generateClosedDoubleTemplate = (
   sex,
   numOfCurrent,
-  numOfOpened
+  numOfClosed
 ) => {
   switch (sex) {
     case config.sex.man:
-      return `Dziękujemy za zgłoszenie ${numOfCurrent} Poruszane przez Pana kwestie zostały już wyjaśnione w odpowiedzi na zgłoszenie ${numOfOpened}.
+      return `Dziękujemy za zgłoszenie ${numOfCurrent} Poruszane przez Pana kwestie zostały już wyjaśnione w odpowiedzi na zgłoszenie ${numOfClosed}.
 
 Z poważaniem, 
 Obsługa Klienta Play`;
     case config.sex.woman:
-      return `Dziękujemy za zgłoszenie ${numOfCurrent}. Poruszane przez Panią kwestie zostały już wyjaśnione w odpowiedzi na zgłoszenie ${numOfOpened}.
+      return `Dziękujemy za zgłoszenie ${numOfCurrent}. Poruszane przez Panią kwestie zostały już wyjaśnione w odpowiedzi na zgłoszenie ${numOfClosed}.
       
 Z poważaniem, 
 Obsługa Klienta Play`;
     case config.sex.company:
-      return `Dziękujemy za zgłoszenie ${numOfCurrent}. Poruszane przez Państwa kwestie zostały już wyjaśnione w odpowiedzi na zgłoszenie ${numOfOpened}.
+      return `Dziękujemy za zgłoszenie ${numOfCurrent}. Poruszane przez Państwa kwestie zostały już wyjaśnione w odpowiedzi na zgłoszenie ${numOfClosed}.
 
 Z poważaniem,
 Obsługa Klienta Play`;
@@ -26,4 +26,4 @@ Obsługa Klienta Play`;
   }
 };
 
-export default generateOpenedDoubleTemplate;
+export default generateClosedDoubleTemplate;
