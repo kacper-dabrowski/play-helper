c9cb832372d5224030d6b28ae4bd6de781073581
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-02-03 19:17:54 +0100

    9 - Add click listener to backdrop

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyledBackdrop } from './StyledBackdrop';
 
-const Backdrop = ({ isOpened, children }) => {
+const Backdrop = ({ isOpened, children, closeModalHandler }) => {
     if (!isOpened) {
         return null;
     }
 
-    return <StyledBackdrop>{children}</StyledBackdrop>;
+    return <StyledBackdrop onClick={closeModalHandler}>{children}</StyledBackdrop>;
 };
 
 export default Backdrop;
