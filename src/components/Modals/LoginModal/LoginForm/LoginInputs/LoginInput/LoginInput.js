a8b473347d92d9bff274138cd3eab03074096727
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-02-04 16:22:11 +0100

    6 - Add focus on modal opening and srq search

--- src/components/Modals/LoginModal/LoginForm/LoginInputs/LoginInput/LoginInput.js
+++ src/components/Modals/LoginModal/LoginForm/LoginInputs/LoginInput/LoginInput.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { StyledLoginInput } from './StyledLoginInput';
 
-const LoginInput = (props) => {
-    return (
-        <>
-            <StyledLoginInput {...props} />
-        </>
-    );
+const LoginInput = ({ focusRef, ...props }) => {
+    return <StyledLoginInput ref={focusRef} {...props} />;
 };
 
 export default LoginInput;
--- src/containers/Support/Srq/SrqFinder/SrqSearchbar/SrqSearchbar.js
+++ src/containers/Support/Srq/SrqFinder/SrqSearchbar/SrqSearchbar.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import useFocus from '../../../../../hooks/useFocus';
 import { StyledInputWrapper, StyledSrqSearchbar } from './StyledSrqSearchbar';
 
 const SrqSearchbar = ({ onType, value }) => {
+    const focusRef = useFocus();
     return (
         <StyledInputWrapper>
             <StyledSrqSearchbar
+                ref={focusRef}
                 value={value}
                 onChange={(event) => {
                     onType(event);
