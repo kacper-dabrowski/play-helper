f2e151450aa57fad9d8f2c5aac71a2f4bfc03a96
Kacper Dąbrowski <kdtw301@gmail.com>
2021-03-18 19:40:27 +0100

    Healthcheck - Add healthcheck

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router';
+import axios from 'axios';
 import Logout from './components/Logout/Logout';
 import PrivateRoute from './components/Routes/PrivateRoute/PrivateRoute';
 import PlayNext from './Pages/PlayNext/PlayNext';
@@ -10,9 +11,11 @@ import UserPanel from './Pages/UserPanel/UserPanel';
 import routes from './shared/routes';
 import * as actions from './store/actions';
 import NotFoundProviderSwitch from './components/Routes/NotFoundProviderSwitch/NotFoundProviderSwitch';
+import urls from './shared/urls';
 
 const App = (props) => {
     useEffect(() => {
+        axios.get(urls.healthcheck);
         props.onTryAutoSignup();
     });
 
