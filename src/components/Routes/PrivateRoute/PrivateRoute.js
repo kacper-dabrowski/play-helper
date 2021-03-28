ec75227dbb20691c53024388cbf34528c000ab5b
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-03-28 23:33:15 +0200

    Auth - Fix undefined route error

--- src/components/Routes/PrivateRoute/PrivateRoute.js
+++ src/components/Routes/PrivateRoute/PrivateRoute.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router';
+import Logout from '../../Logout/Logout';
 
 const PrivateRoute = ({ isAuthenticated, ...props }) => {
-    return isAuthenticated && <Route {...props}>{props.children}</Route>;
+    return isAuthenticated ? <Route {...props}>{props.children}</Route> : <Logout />;
 };
 
 const mapStateToProps = (state) => ({
