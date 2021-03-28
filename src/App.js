b3a357a9888ff3bf3d7cd6ccd7dececa36c91d8b
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-03-28 23:33:15 +0200

    Auth - Fix undefined route error

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
+import axios from 'axios';
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router';
-import axios from 'axios';
 import SnackbarProvider from 'react-simple-snackbar';
 import Logout from './components/Logout/Logout';
+import NotFoundProviderSwitch from './components/Routes/NotFoundProviderSwitch/NotFoundProviderSwitch';
 import PrivateRoute from './components/Routes/PrivateRoute/PrivateRoute';
 import PlayNext from './Pages/PlayNext/PlayNext';
 import Support from './Pages/Support/Support';
-import WelcomeScreen from './Pages/WelcomeScreen/WelcomeScreen';
 import UserPanel from './Pages/UserPanel/UserPanel';
+import WelcomeScreen from './Pages/WelcomeScreen/WelcomeScreen';
 import routes from './shared/routes';
-import * as actions from './store/actions';
-import NotFoundProviderSwitch from './components/Routes/NotFoundProviderSwitch/NotFoundProviderSwitch';
 import urls from './shared/urls';
+import * as actions from './store/actions';
 
 const App = (props) => {
     useEffect(() => {
