a6a611c13d812a6b2a9bf6404ae452872b02293a
Kacper Dąbrowski <kdtw301@gmail.com>
2021-04-05 18:52:45 +0200

    Textarea fixes

--- src/components/TextArea/StyledTextarea.js
+++ src/components/TextArea/StyledTextarea.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const StyledTextArea = styled.textarea`
     border-radius: 10px;
     width: 40%;
+    resize: none;
     height: 10rem;
     padding: 0.5rem;
     font: inherit;
