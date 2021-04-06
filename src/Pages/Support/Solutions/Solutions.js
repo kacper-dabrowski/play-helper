27ceb09b2f640cc0b6bfba6d681c41335d2d9930
Kacper Dąbrowski <kdtw301@gmail.com>
2021-04-06 18:57:13 +0200

    Add searching feature to custom notifications

--- src/Pages/Support/Solutions/Solutions.js
+++ src/Pages/Support/Solutions/Solutions.js
@@ -13,36 +13,62 @@ const Solutions = () => {
     const [template, setTemplate] = useState('');
     const [response, error, loading] = useRequest(urls.solution, null, REQUEST_METHODS.GET);
     const [, setError] = useFeedbackSnackbars();
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    const solutions = response?.data || [];
+
+    const normalizeSolutions = ({ title, description, content, isPublic, _id }) => (
+        <SolutionResult
+            clickable
+            title={title}
+            description={description}
+            key={_id}
+            content={content}
+            setTemplate={setTemplate}
+            isPublic={isPublic}
+        />
+    );
 
     useEffect(() => {
         if (error) {
             setError(error.message);
         }
     }, [error]);
+    const onSearch = (searchPhrase) => {
+        const search = solutions.filter((result) => {
+            return (
+                result.title.toLowerCase().includes(searchPhrase) ||
+                result.description.toLowerCase().includes(searchPhrase) ||
+                result.content.toLowerCase().includes(searchPhrase)
+            );
+        });
+        setSearchResults(search);
+    };
 
-    const solutions = response?.data || [];
+    const searchSolutionHandler = (event) => {
+        setSearchQuery(event.target.value);
+        onSearch(event.target.value);
+    };
+
     let results;
 
     if (loading) {
         results = <Spinner centered />;
+    }
+    if (!searchQuery) {
+        results = solutions.map(normalizeSolutions);
     } else {
-        results = solutions.map(({ title, description, content, isPublic, _id }) => (
-            <SolutionResult
-                clickable
-                title={title}
-                description={description}
-                key={_id}
-                content={content}
-                setTemplate={setTemplate}
-                isPublic={isPublic}
-            />
-        ));
+        results = searchResults.map(normalizeSolutions);
+    }
+    if (results.length === 0) {
+        results = <p>Brak wyników</p>;
     }
 
     return (
         <>
             <SolutionResults>
-                <Searchbar />
+                <Searchbar onType={searchSolutionHandler} value={searchQuery} />
                 <StyledResults>{results}</StyledResults>
             </SolutionResults>
             <MainTextarea setTemplate={setTemplate} value={template} />
