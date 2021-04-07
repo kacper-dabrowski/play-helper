101552fa1b7917ddf098038f2c2cffb5a08a4f69
Kacper Dąbrowski <kdtw301@gmail.com>
2021-04-07 19:17:41 +0200

    13 - Refactor SrqFinder not to fetch data on its own

--- src/Pages/Support/Srq/Srq.js
+++ src/Pages/Support/Srq/Srq.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import MainTextarea from '../../../components/MainTextarea/MainTextarea';
 import SrqFinder from '../../../components/SrqFinder/SrqFinder';
+import useRequest from '../../../hooks/useRequest';
+import urls from '../../../shared/urls';
 
 const Srq = () => {
     const [template, setTemplate] = useState('');
+    const [response, error, loading] = useRequest(urls.srq, 'GET', null);
+
     return (
         <>
-            <SrqFinder setTemplate={setTemplate} clickable />
+            <SrqFinder setTemplate={setTemplate} clickable response={response} error={error} loading={loading} />
             <MainTextarea value={template} setTemplate={setTemplate} />
         </>
     );
