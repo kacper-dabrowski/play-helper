db8991c66ff3eac2aa2ea8762700e0ef89cf8e5e
Kacper Dąbrowski <kdtw301@gmail.com>
2021-04-23 09:18:50 +0200

    16 - Fix 404 page display on private routes

--- src/components/Routes/PrivateRoute/PrivateRoute.js
+++ src/components/Routes/PrivateRoute/PrivateRoute.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
 const PrivateRoute = ({ isAuthenticated, ...props }) => {
-    return isAuthenticated ? <Route {...props}>{props.children}</Route> : null;
+    return isAuthenticated ? <Route {...props}>{props.children}</Route> : <NotFoundPage />;
 };
 
 const mapStateToProps = (state) => ({
