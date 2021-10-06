37ac5d8c61959b825611bb2a02142a93d7ede849
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-10-06 22:15:05 +0200

    Add tests for next templates

--- src/containers/layouts/SupportLayout/SupportLayout.js
+++ src/containers/layouts/SupportLayout/SupportLayout.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import * as Styled from './StyledSupportLayout';
 import Navbar from '../../../components/UI/Navbars/PageNavbar/Navbar';
 import { BaseLayout } from '../BaseLayout/BaseLayout';
+import { useStore } from '../../../hooks/useStore';
 
 const SupportLayout = ({ children, routes, backgroundImage }) => {
-    const fullName = useSelector((state) => state.auth.user.fullName);
+    const { authStore } = useStore();
 
     return (
         <>
             <BaseLayout backgroundImage={backgroundImage}>
-                <Navbar routes={routes} username={fullName} />
+                <Navbar routes={routes} username={authStore.user.fullName} />
                 <Styled.Container>{children}</Styled.Container>
             </BaseLayout>
         </>
