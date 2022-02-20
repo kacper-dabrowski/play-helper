0ad3ae7b94a88c4d31b8d18b252ee53bd27c02e0
Kacper Dąbrowski <kacper@couponfollow.com>
2022-02-20 14:33:26 +0100

    Move logout to authorization module and add tests for it

--- src/authorization/logout/logout.tsx
+++ src/authorization/logout/logout.tsx
@@ -2,19 +2,22 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router';
 import { toastProvider } from '../../libs/toast';
-import { logout } from '../../authorization/store/auth';
+import { logout } from '../store/auth';
+import { StoreState } from '../../stores/store';
 
-const Logout = () => {
-    const logoutTimeoutId = useSelector((state) => state.auth.logoutTimeoutId);
+export const Logout = () => {
+    const logoutTimeoutId = useSelector((state: StoreState) => state.auth.logoutTimeoutId);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        clearTimeout(logoutTimeoutId);
+        if (logoutTimeoutId) {
+            clearTimeout(logoutTimeoutId);
+        }
+
         dispatch(logout());
+
         toastProvider.info('Zostałeś wylogowany');
     });
 
     return <Redirect to="/" />;
 };
-
-export default Logout;
