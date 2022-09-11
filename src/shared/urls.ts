d5c4871273218bcf64ce7e9bdb0c31c7749a0dcb
kacper-cf <kacper@couponfollow.com>
2022-09-11 23:04:46 +0200

    update production url

--- src/shared/urls.ts
+++ src/shared/urls.ts
@@ -1,5 +1,5 @@
 const API_URL =
-    process.env.NODE_ENV === 'production' ? 'https://play-helper-api.herokuapp.com' : 'http://localhost:3001';
+    process.env.NODE_ENV === 'production' ? 'https://play-helper-api.onrender.com' : 'http://localhost:3001';
 
 const urls = {
     signup: `${API_URL}/signup`,
